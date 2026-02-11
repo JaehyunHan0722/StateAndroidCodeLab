43322fe99ee770d936a88455fd94944f436eb42e
Jaehyun Han <jhhan@Jaehyuns-MacBook-Pro.local>
2026-02-12 06:52:59 +0900

    Step12: State in ViewModel

--- app/src/main/java/com/jhhan/stateandroidcodelab/WellnessTask.kt
+++ app/src/main/java/com/jhhan/stateandroidcodelab/WellnessTask.kt
@@ -1,3 +1,16 @@
 package com.jhhan.stateandroidcodelab
 
-data class WellnessTask(val id: Int, val label: String, var checked: Boolean = false)
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+
+//data class WellnessTask(val id: Int, val label: String, var checked: Boolean = false)
+//data class WellnessTask(val id: Int, val label: String, var checked: MutableState<Boolean> = mutableStateOf(false))
+class WellnessTask(
+    val id: Int,
+    val label: String,
+    initialChecked: Boolean = false,
+){
+    var checked by mutableStateOf(initialChecked)git 
+}
